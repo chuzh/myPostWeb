d4c9cd8dd85f16d97b4c077abf6407767e76b3f5
chzhang <chu_z@hotmail.com>
2014-07-16 23:09:56 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('myPostWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
