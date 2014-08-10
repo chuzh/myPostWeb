74cb9b286a1a7c18b87bf83111be2ddcdbe41ca9
chzhang <chu_z@hotmail.com>
2014-08-09 23:48:24 -0700

    add ngRepeat on myPostList directive

--- app/scripts/directives/myPostList.js
+++ app/scripts/directives/myPostList.js
@@ -3,15 +3,10 @@ var app = angular.module('myPostWeb.directives', []);
 app.directive('myPostList', function() {
     return {
         restrict: 'AE',
-        replace: 'true',
-        template: '<div>'
-            +'<ul>'
-            +'<li ng-repeat="post in postList">'
-            +'<div>[{{$index + 1}}] {{post.title}}  </div>'
+        replace: true,
+        template: '<div>'+'<div>[{{$index + 1}}] {{post.title}}  </div>'
             +'<div>submitted by {{post.author.name}} /<div>'
-            +'div><a ng-href="#/commentList/{{post._id}}">{{post.comments.length}} comments </a></div>'
-            +'</li>'
-            +'</ul>'
-            +'</div>'
+            +'<div><a ng-href="#/commentList/{{post._id}}">{{post.comments.length}} comments </a></div>'
+          +'</div>'
     };
 });
