f63b73cc3db9e28643584602fb5eb05f76b7d2a0
Chu Zhang <chu.zhang@move.com>
2014-09-29 23:43:03 -0700

    phantomjs

--- e2e/config.js
+++ e2e/config.js
@@ -1,7 +1,19 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js'],
-    multiCapabilities: [{
-        browserName: 'chrome'
-    }]
-}
+    capabilities: {
+        browserName: 'phantomjs',
+        version: '',
+        platform: 'ANY'
+    }
+    // -----------------------------------------------------------------
+    // Browser and Capabilities: Chrome
+    // -----------------------------------------------------------------
+    /*
+    capabilities: {
+        browserName: 'chrome',
+        version: '',
+        platform: 'ANY'
+    }
+    */
+};
