d8890a2f8cd104dc1c42e8a82f1401fbc250f149
Chu Zhang <chu.zhang@move.com>
2014-09-30 22:53:12 -0700

    e2e jasmineNodeOpts

--- e2e/config.js
+++ e2e/config.js
@@ -5,15 +5,21 @@ exports.config = {
         browserName: 'phantomjs',
         version: '',
         platform: 'ANY'
-    }
+    },
     // -----------------------------------------------------------------
     // Browser and Capabilities: Chrome
     // -----------------------------------------------------------------
-    /*
+     /*
     capabilities: {
         browserName: 'chrome',
         version: '',
         platform: 'ANY'
+    },*/
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true, // Use colors in the command line report.
+        isVerbose: true, // List all tests in the console
+        includeStackTrace: true
     }
-    */
+
 };
